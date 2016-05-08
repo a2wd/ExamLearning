d85cba14326e2c62810f6945872676db95254362
a2wd <github@a2wd.com>
2016-05-08 02:11:29 +0100

    Completed chapter 2

---
+++ ExamLearning/Chapter2/2.7.CalculateWorker.js
@@ -0,0 +1,13 @@
+﻿onmessage = function(e) {
+    var result = document.getElementById("workResult");
+    result.innerText = "";
+    var work = 35000000;
+    var i = 0;
+    var a = new Array(work);
+    var sum = 0;
+    for (i = 0; i < work; i++) {
+        a[i] = i * i
+        sum += i * i;
+    }
+    self.postMessage(sum);
+}
